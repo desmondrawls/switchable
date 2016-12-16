07ea68d1937a79e24c776a86f6373db2a0823c6f
Desmond Pompa Alarcon Rawls <drawls@pivotal.io>
2016-12-16 23:32:03 +0100

    working??

--- toggle.js
+++ toggle.js
@@ -3,40 +3,35 @@ createToggleObservable = (initialValue, override) => {
   return createToggle(new Rx.Subject(), initialValue, override);
 }
 createOverrideObservable = initialValue => {
-  return createOverride(new Rx.Subject(), initialValue);
+  return createToggle(new Rx.Subject(), initialValue, null);
 }
 
+createToggle = (subject, initialValue, override) => { 
+  overriddenSubscribe = callback => {
+    subject
+        .withLatestFrom(override.subject)
+        .do(([current, override]) => console.log("overridden gets first: ", current, "and ", override))
+        .map(([current, override]) => { return !current && !override })
+        .startWith("start overridden")
+        .subscribe(callback)
+  }
 
-createOverride = (subject, initialValue) => {
+  plainSubscribe = callback => subject.startWith(initialValue).subscribe(callback)
+  
   return {
     subject: subject,
     toggle: () => subject.next(!initialValue),
-    subscribe: (callback) => subject.startWith(initialValue).subscribe(callback)
-  }
-}
-
-
-createToggle = (subject, initialValue, override) => {
-  return {
-    toggle: () => subject.next(!initialValue),
     subscribe: (callback) => {
-      subject
-        .startWith(initialValue)
-        .withLatestFrom(override.subject)
-        .map(([current, override]) => { return !current && !override })
-        .subscribe(callback)
+      override ? overriddenSubscribe(callback) : plainSubscribe(callback)
     }
   }
 }
 
 const override = createOverrideObservable(false)
 const toggle = createToggleObservable(false, override);
-toggle.subscribe(x => console.log("TOGGLE: ", x))
 override.subscribe(x => console.log("OVERRIDE Junk: ", x))
+toggle.subscribe(x => console.log("TOGGLE: ", x))
 override.toggle()
-toggle.toggle()
-override.toggle()
-toggle.toggle()
 toggle.toggle()
 
 function createFalseObservable(){
@@ -45,4 +40,5 @@ function createFalseObservable(){
 function createTrueObservable(){
   return new Rx.Observable.interval(1000).map(x => true).take(10)
 }
+
 
