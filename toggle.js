620668dafae8d4921a034699522a66814a9d0a40
Desmond Pompa Alarcon Rawls <drawls@pivotal.io>
2016-12-17 21:43:01 +0100

    ***Multiple overrides***

--- toggle.js
+++ toggle.js
@@ -1,54 +1,60 @@
-
-const initiateToggle = (initialValue) => {
-  return createToggle(new Rx.Subject(), initialValue, null);
+const initializeToggle = (initialValue) => {
+  return createToggle(new Rx.Subject(), initialValue, []);
 }
 
-const createToggle = (subject, initialValue, override) => { 
+const createToggle = (subject, initialValue, parents) => { 
   const isVisibleWhenParentNot = () => {
-    return Rx.Observable.merge(childStream(), parentStream())
+    return Rx.Observable.merge(childStream(), ...parents.map((parent) => parentStream(parent)))
   }
   
   const childStream = () => {
     return subject
-        .withLatestFrom(override.isVisible())
-        .map(([child, parent]) => { return child && !parent })
+        .withLatestFrom(...parents.map((parent) => parent.isVisible()))
+        .map(([child, ...parentVisibilities]) => { 
+          return child && parentVisibilities.every((x) => x !== true);
+        })
   }
   
-  const parentStream = () => {
-    return override.isVisible()
+  const parentStream = (parent) => {
+    return parent.isVisible()
             .withLatestFrom(subject)
             .map(([parent, child]) => { return child && !parent })
   }
 
   return {
     override: (parent) => {
-      return new createToggle(subject, initialValue, parent);
+      return new createToggle(subject, initialValue, parents.concat(parent));
     }, 
     subject: subject,
     toggle: () => {
       subject.next(!initialValue)
-      return new createToggle(subject, !initialValue, override);
+      return new createToggle(subject, !initialValue, parents);
     },
     isVisible: () => {
-      return override ? isVisibleWhenParentNot() : subject
+      return parents.length ? isVisibleWhenParentNot() : subject
     }
   }
 }
 
-const grandparent = initiateToggle(true)
-const parent = initiateToggle(true).override(grandparent)
-const child = initiateToggle(true).override(parent);
+const grandparent = initializeToggle(true)
+const parent = initializeToggle(true).override(grandparent)
+const otherParent = initializeToggle(true)
+const child = initializeToggle(true).override(parent).override(otherParent);
 grandparent.isVisible().subscribe(x => console.log("GRANDPARENT VISIBLE: ", x))
 parent.isVisible().subscribe(x => console.log("PARENT VISIBLE: ", x))
+otherParent.isVisible().subscribe(x => console.log("OTHER PARENT VISIBLE: ", x))
 child.isVisible().subscribe(x => console.log("CHILD VISIBLE: ", x))
 const grandparentOff = grandparent.toggle()
 const parentOff = parent.toggle()
-console.log("child sequence")
-const childOff = child.toggle().toggle().toggle()
+const otherParentOff = otherParent.toggle()
+console.log("child twice")
+const childOff = child.toggle().toggle()
 console.log("parent once")
 parentOff.toggle()
 console.log("grandparent once")
 grandparentOff.toggle()
+console.log("other parent twice")
+otherParentOff.toggle().toggle()
 console.log("final toggle")
 childOff.toggle()
 
